4bc12ba8e9294f7663fa7834dc2a56f78859d337
LordMahir <mahirjain04@gmail.com>
2022-12-30 15:04:18 +0530

    First

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import { loadTodos } from "../stores/todoStore.js";
+	import { supabase } from "../supabase";
+    console.log(supabase);
+
+    
+</script>
+
+<div class="container mx-auto my-6 max-w-lg">        
+    <slot></slot>
+</div>
